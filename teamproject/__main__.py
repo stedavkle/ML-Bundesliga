aa20dcd994aeb466e6ff4c60a4841ac0210061c9
Stephandi94 <stephan.amann@student.uni-tuebingen.de>
2021-05-21 16:16:31 +0200

    GUI Basis erstellt, __main__.py auf gui.py gelinked

--- teamproject/__main__.py
+++ teamproject/__main__.py
@@ -3,5 +3,6 @@ Run the application.
 
 This module is invoked when calling ``python -m teamproject``.
 """
-from teamproject.gui import main
+#from teamproject.gui import main
+from gui import main
 main()
--- teamproject/gui.py
+++ teamproject/gui.py
@@ -1,20 +1,51 @@
 """
-Add your GUI code here.
+web gui
 """
 
-from teamproject.crawler import fetch_data
-from teamproject.models import ExperienceAlwaysWins
+import eel
 
+"""Initialize Browser Presettings"""
+my_options = {
+    'host': 'localhost',
+    'port': '8080'
+}
+
+""" TEST DATA """
+clubs = {   19: "Bayern München",
+            93: "VfB Stuttgart",
+            17: "Borussia M'Gladbach",
+            100: "RB Leipzig",
+            9: "Borussia Dortmund",
+            1: "VfL Wolfsburg",
+            24: "Werder Bremen",
+            7: "SC Freiburg",
+            8: "FC Augsburg",
+            42: "Arminia Bielefeld",
+            2: "1. FC Heidenheim"}
+
+"""directory where web files located"""
+eel.init('web')
+
+""" FUNCTION COLLECTION """
+
+# TODO: Crawler Call
+@eel.expose
+def start_crawler():
+    print("start_crawler executed")
+    # Crawler wird aufgerufen
+
+
+@eel.expose
+def get_clubs(league):
+    return clubs
+
+
+# TODO: Algo aufrufen und Parameter übergeben
+@eel.expose
+def start_algo(team1, team2):
+    result = [team1, team2, '60%', '10%', '30%']
+    print(result)
+    return result
 
 def main():
-    """
-    Creates and shows the main window.
-    """
-    # Add code here to create and initialize window.
-
-    # For demo purposes, this is how you could access methods from other
-    # modules:
-    data = fetch_data()
-    model = ExperienceAlwaysWins(data)
-    winner = model.predict_winner('Tübingen', 'Leverkusen')
-    print(winner)
+    eel.start('index.html', size=(600, 400))  # landing page of gui and window size
