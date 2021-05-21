a34fe78de768404f51814143a10aae282dccc917
David Kleindiek <david.kleindiek@gmail.com>
2021-05-22 00:03:59 +0200

    added basic algorithm, sumMostGoalsIsWinner()

--- teamproject/models.py
+++ teamproject/models.py
@@ -3,6 +3,25 @@ This module contains code for a prediction models.
 """
 
 from collections import Counter
+import pandas as pd
+
+class sumMostGoalsIsWinner:
+    def __init__(self, results):
+        self.end_results = results[results['ResultTypeID'] == 2]
+        self.team1_id = results.loc[0,'Team1.TeamId']
+        self.team2_id = results.loc[0, 'Team2.TeamId']
+        self.team1_is_team1_id_results = self.end_results[self.end_results['Team1.TeamId'] == self.team1_id]
+        self.team1_is_team2_id_results = self.end_results[self.end_results['Team1.TeamId'] == self.team2_id]
+
+        self.goals_team1_id = self.team1_is_team1_id_results['PointsTeam1'].sum(axis=0) + self.team1_is_team2_id_results['PointsTeam2'].sum(axis=0)
+        self.goals_team2_id = self.team1_is_team1_id_results['PointsTeam2'].sum(axis=0) + self.team1_is_team2_id_results['PointsTeam1'].sum(axis=0)
+
+    def predict_winner(self):
+        if self.goals_team1_id > self.goals_team2_id:
+            return self.team1_id
+        if self.goals_team1_id < self.goals_team2_id:
+            return self.team2_id
+        return -1
 
 
 class ExperienceAlwaysWins:
@@ -25,3 +44,12 @@ class ExperienceAlwaysWins:
             return home_team
         else:
             return guest_team
+
+# for testing inside the script
+if __name__ == '__main__':
+    from crawler import getMatchupHistoryFromAPI
+
+    matches, match_results, match_goals = getMatchupHistoryFromAPI(16,87)
+    algo = sumMostGoalsIsWinner(match_results)
+    print(algo.predict_winner())
+
