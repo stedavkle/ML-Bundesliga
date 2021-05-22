4c152960cda49d32bf2974ad33779412fde14f78
Stephandi94 <stephan.amann@student.uni-tuebingen.de>
2021-05-22 10:41:07 +0200

    add visibility_functions.js to GUI

---
+++ teamproject/web/visibility_functions.js
@@ -0,0 +1,7 @@
+function display_welcome_page(){
+    document.getElementById("welcome_page").style.display = 'block';
+}
+
+function hide_welcome_page(){
+    document.getElementById("welcome_page").style.display = 'none';
+}
