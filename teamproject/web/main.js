754854f00c195ba7f3fcf2ff3cd13cf58fc083fe
Amelie Schäfer <amelie.schaefer@student.uni-tuebingen.de>
2021-05-22 11:50:36 +0200

    added dom_manipulation.js and refactored main.js

--- teamproject/web/main.js
+++ teamproject/web/main.js
@@ -35,7 +35,8 @@ function list_algorithms(){
     for (var key in algorithms){
         html_str += "<option value='" + key + "'>" + algorithms[key] + "</option>";
     }
-    document.getElementById("algo_selection_dummy").innerHTML = html_str;
+    set_innerHTML("algo_selection_dummy", html_str);
+    //document.getElementById("algo_selection_dummy").innerHTML = html_str;
 
 
 
@@ -43,11 +44,17 @@ function list_algorithms(){
 
 function show_select_algo_submit(){
     selected_algo_id = document.getElementById("algo_selection").value;
-    var algo_submit = document.getElementById("algo_submit");
+    //var algo_submit = document.getElementById("algo_submit");
 
     if (selected_algo_id != 0){
-        algo_submit.style.display = 'block';
-        algo_submit.innerHTML = "<button onclick='select_algo()'>Bestätigen</button>";
+
+        display("algo_submit");
+        set_innerHTML("algo_submit", "<button onclick='select_algo()'>Bestätigen</button>")
+
+        //document.getElementById("algo_submit").innerHTML = "<button onclick='select_algo()'>Bestätigen</button>";
+        //algo_submit.style.display = 'block';
+        //algo_submit.innerHTML = "<button onclick='select_algo()'>Bestätigen</button>";
+
     }
 }
 
@@ -60,13 +67,16 @@ function select_algo(){
     // in case of restarting program
     //erase_data();
 
-    document.getElementById("welcome_page").style.display = 'none';
+    hide("welcome_page");
+    //document.getElementById("welcome_page").style.display = 'none';
     selected_algo_data = 'data_' + selected_algo_id;
-    document.getElementById(selected_algo_data).style.display = 'block';
+    display(selected_algo_data);
+    //document.getElementById(selected_algo_data).style.display = 'block';
 
     list_seasons();
     list_leagues();
-    document.getElementById("back_button").innerHTML = "<button onclick='go_to_start()'>zurück</button>";
+    set_innerHTML("back_button", "<button onclick='go_to_start()'>zurück</button>");
+    //document.getElementById("back_button").innerHTML = "<button onclick='go_to_start()'>zurück</button>";
     //document.getElementById("test").innerHTML = selected_algo_data;
 
 
@@ -74,16 +84,19 @@ function select_algo(){
 }
 
 function list_seasons(){
-    document.getElementById("select_seasons").innerHTML = "<p>Hier wird man die Saisons auswählen können...</p>";
+    set_innerHTML("select_seasons", "<p>Hier wird man die Saisons auswählen können...</p>");
+    //document.getElementById("select_seasons").innerHTML = "<p>Hier wird man die Saisons auswählen können...</p>";
 }
 
 function list_leagues(){
-    document.getElementById("select_league").innerHTML = "<p>Hier wird man die Liga auswählen können...</p>";
+    set_innerHTML("select_league", "<p>Hier wird man die Liga auswählen können...</p>");
+    //document.getElementById("select_league").innerHTML = "<p>Hier wird man die Liga auswählen können...</p>";
 }
 
 
 function start_crawler(){
-    document.getElementById(selected_algo_data).style.display = 'none';
+    hide(selected_algo_data);
+    //document.getElementById(selected_algo_data).style.display = 'none';
     // TODO: sich etwas zum Thema Übergabe von Auswahl überlegen
     eel.start_crawler()(callback_start_crawler)
 
@@ -92,15 +105,20 @@ function start_crawler(){
 
 // -------------------- training function ------------------------------------------------------------
 function callback_start_crawler(){
-    document.getElementById("training").style.display = 'block';
-    document.getElementById("back_button").innerHTML = "<button onclick='back_to_data_selection()'>zurück</button>"
+    display("training");
+    //document.getElementById("training").style.display = 'block';
+    set_innerHTML("back_button", "<button onclick='back_to_data_selection()'>zurück</button>");
+    //document.getElementById("back_button").innerHTML = "<button onclick='back_to_data_selection()'>zurück</button>"
 }
 
 // -------------------- match selection function ------------------------------------------------------------
 function get_clubs(){
-    document.getElementById("training").style.display = 'none';
-    document.getElementById("back_button").innerHTML = "<button onclick='back_to_data_selection()'>zurück</button>"
-    document.getElementById("opponent_buttons").style.display = 'block';
+    hide("training");
+    //document.getElementById("training").style.display = 'none';
+    set_innerHTML("back_button", "<button onclick='back_to_data_selection()'>zurück</button>");
+    //document.getElementById("back_button").innerHTML = "<button onclick='back_to_data_selection()'>zurück</button>"
+    display("opponent_buttons");
+    //document.getElementById("opponent_buttons").style.display = 'block';
     eel.get_clubs(selected_league)(callback_select_home)
     //callback_select_home(1);
     //eel.club_select('"sel1"')(callback_home);
@@ -114,51 +132,66 @@ function callback_select_home(clubs){
     for (var key in team_list){
         html_str += "<option value='" + key + "'>" + team_list[key] + "</option>";
     }
-    document.getElementById("clubs_home").innerHTML = html_str;
-    document.getElementById("match_selection").style.display = 'block';
+    set_innerHTML("clubs_home", html_str);
+    //document.getElementById("clubs_home").innerHTML = html_str;
+    display("match_selection");
+    //document.getElementById("match_selection").style.display = 'block';
+
     //document.getElementById("clubs_home").innerHTML=clubs;
 }
 
 // TODO: Anfrage beim Crawler
 function get_next_opponent(){
     if (document.getElementById("sel1").value != 0){
-        document.getElementById("opponent_buttons").style.display = 'none';
-        document.getElementById("clubs_guest").style.display = 'block';
-        document.getElementById("hint").innerHTML = '';
+        hide("opponent_buttons");
+        //document.getElementById("opponent_buttons").style.display = 'none';
+        display("clubs_guest");
+        //document.getElementById("clubs_guest").style.display = 'block';
+        set_innerHTML("hint", '');
+        //document.getElementById("hint").innerHTML = '';
         // TODO: hier muss dann der nächste Gegner gewählt werden
         // dummy function
             team2 = team1;
             team2_id = team1_id;
             document.getElementById("clubs_guest").innerHTML = team1;
 
-        document.getElementById("submit_buttons").style.display = 'block';
-        document.getElementById("switch_button").innerHTML = "<button onclick='select_opponent()'>anderen Gegner wählen</button><button onclick='start_algo()'>Bestätigen</button>"
+        display("submit_buttons");
+        //document.getElementById("submit_buttons").style.display = 'block';
+        set_innerHTML("switch_button", "<button onclick='select_opponent()'>anderen Gegner wählen</button><button onclick='start_algo()'>Bestätigen</button>");
+        //document.getElementById("switch_button").innerHTML = "<button onclick='select_opponent()'>anderen Gegner wählen</button><button onclick='start_algo()'>Bestätigen</button>"
     }
     else {
         // TODO: alert toast
-        document.getElementById("hint").innerHTML = "Wähle zuerst ein Team!";
+        set_innerHTML("hint", "Wähle zuerst ein Team!");
+        //document.getElementById("hint").innerHTML = "Wähle zuerst ein Team!";
     }
 }
 
 function select_opponent(){
     if (document.getElementById("sel1").value != 0){
-        document.getElementById("opponent_buttons").style.display = 'none';
-        document.getElementById("hint").innerHTML = '';
+        hide("opponent_buttons");
+        //document.getElementById("opponent_buttons").style.display = 'none';
+        set_innerHTML("hint", '');
+        //document.getElementById("hint").innerHTML = '';
 
         var html_str = "<select id='sel2' onchange='set_team(2)'><option value='0' selected disabled hidden>kein Team ausgewählt</option>";
         for (var key in team_list){
             html_str += "<option value='" + key + "'>" + team_list[key] + "</option>";
         }
 
-        var clubs_guest = document.getElementById("clubs_guest");
-        clubs_guest.style.display = 'block';
-        clubs_guest.innerHTML = html_str;
-        document.getElementById("submit_buttons").style.display = 'block';
-        document.getElementById("switch_button").innerHTML = "<button onclick='get_next_opponent()'>nächster Gegner</button><button onclick='start_algo()'>Bestätigen</button>"
+        display("clubs_guest");
+        //document.getElementById("clubs_guest").style.display = 'block';
+        set_innerHTML("clubs_guest", html_str);
+        //document.getElementById("clubs_guest").innerHTML = html_str;
+        display("submit_buttons");
+        //document.getElementById("submit_buttons").style.display = 'block';
+        set_innerHTML("switch_button", "<button onclick='get_next_opponent()'>nächster Gegner</button><button onclick='start_algo()'>Bestätigen</button>");
+        //document.getElementById("switch_button").innerHTML = "<button onclick='get_next_opponent()'>nächster Gegner</button><button onclick='start_algo()'>Bestätigen</button>"
     }
     else {
         // TODO: alert toast
-        document.getElementById("hint").innerHTML = "Wähle zuerst ein Team!";
+        set_innerHTML("hint", "Wähle zuerst ein Team!");
+        //document.getElementById("hint").innerHTML = "Wähle zuerst ein Team!";
     }
 }
 
@@ -194,24 +227,28 @@ function set_team(sel){
 function start_algo(){
 
     if (team1_id == 0 || team2_id == 0 ){
-        document.getElementById("hint").innerHTML = "Es müssen zwei Clubs ausgewählt werden!";
+        set_innerHTML("hint", "Es müssen zwei Clubs ausgewählt werden!");
+        //document.getElementById("hint").innerHTML = "Es müssen zwei Clubs ausgewählt werden!";
     }
     else if (team1_id == team2_id){
         // TODO: Alert einbauen
-        document.getElementById("hint").innerHTML = "Es müssen unterschiedliche Clubs ausgewählt werden!";
+        set_innerHTML("hint", "Es müssen unterschiedliche Clubs ausgewählt werden!");
+        //document.getElementById("hint").innerHTML = "Es müssen unterschiedliche Clubs ausgewählt werden!";
     }
     else {
         // TODO: Übergabe definieren
         eel.start_algo(team1, team2)(callback_result);
-        document.getElementById("match_selection").style.display = 'none';
+        hide("match_selection");
+        //document.getElementById("match_selection").style.display = 'none';
     }
 
 }
 
 function callback_result(result){
-    document.getElementById("back_button").style.display = 'none';
-    var id_result = document.getElementById("result");
-    id_result.style.display = 'block';
+    hide("back_button");
+    //document.getElementById("back_button").style.display = 'none';
+    display("result");
+    //document.getElementById("result").style.display = 'block';
 
     // just for testing
     var t1 = result[0];
@@ -224,7 +261,7 @@ function callback_result(result){
                     <p>" + h + " x " + d + " x " + g + "</p> \
                     <button onclick='go_to_start()'>zurück zur Startseite</button>\
                     <button onclick='back_to_match_selection()'>anderes Match wählen</button>";
-    id_result.innerHTML = html_str;
+    set_innerHTML("result", html_str);
 
     //document.getElementById("back_to_match_selection").innerHTML = "<button onclick='back_to_match_selection()'>anderes Match wählen</button>";
     //document.getElementById("go_to_start").innerHTML = "<button onclick='go_to_start()'>zurück zur Startseite</button>";
--- teamproject/web/visibility_functions.js
+++
@@ -1,7 +0,0 @@
-function display_welcome_page(){
-    document.getElementById("welcome_page").style.display = 'block';
-}
-
-function hide_welcome_page(){
-    document.getElementById("welcome_page").style.display = 'none';
-}
